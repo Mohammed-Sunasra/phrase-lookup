eefa8c62eee85016cd84c9c0802d594d070f5160
Mohammed-Sunasra <sunasramohd@gmail.com>
2019-04-28 13:15:29 +0530

    Adding AWS notebooks and updating latest code

--- src/config/path.py
+++ src/config/path.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-version = 1
+version = 2
 data_path = Path("data")
 output_path = Path("output_files")
 model_path = Path("model_files")
@@ -10,7 +10,7 @@ train_path = data_path / 'train.csv'
 test_path = data_path /'test.csv'
 med_path = data_path / 'meddra.csv'
 
-TOKENIZER = output_path / 'tokenizer.pkl'
+TOKENIZER = output_path / f'tokenizer_{version}.pkl'
 DATA_OUTPUT_CSV = output_path / 'data_modified.csv'
 MODEL_WEIGHTS_PATH = model_path / f'model_lstm_{version}.h5'
 MODEL_JSON_PATH = model_path / f'model_lstm_{version}.json'
--- src/train.py
+++ src/train.py
@@ -2,23 +2,21 @@ from keras.optimizers import RMSprop
 from datareader import DataReader
 from tokenizer import Tokenize
 from model import LSTMModel
-
 from config import constants as const
 from config import path
+from keras.metrics import top_k_categorical_accuracy, categorical_accuracy
 
 data_reader = DataReader(path.train_path, path.test_path, path.med_path)
-
-#data_bunch = DataReader(path.ICSR_YES, path.ICSR_NO)
 data_reader.save(path.DATA_OUTPUT_CSV)
 
-reader = Reader(data_reader, max_words=const.MAX_WORDS, max_len=const.MAX_LENGTH)
+data_tokenizer = Tokenize(data_reader, max_words=const.MAX_WORDS, max_len=const.MAX_LENGTH)
+data_tokenizer.save_tokenizer(path.TOKENIZER)
 
-reader.save_tokenizer(path.TOKENIZER)
-
-lstm_model = LSTMModel(reader, loss='categorical_crossentropy',
-                       optimizer='adam', metrics=['categorical_accuracy'],
+lstm_model = LSTMModel(data_tokenizer, loss='categorical_crossentropy',
+                       optimizer='adam', metrics=[categorical_accuracy, top_k_categorical_accuracy],
                        batch_size=const.BATCH_SIZE, epochs=const.EPOCHS)
+
 lstm_model.fit()
 
-lstm_model.save(path.MODEL_JSON, path.MODEL_WEIGHTS)
+lstm_model.save(path.MODEL_JSON_PATH, path.MODEL_WEIGHTS_PATH)
 
