5c8edf58d91733c01f85cf51b9335a55db6a30a5
Mohammed-Sunasra <sunasramohd@gmail.com>
2019-04-28 15:57:53 +0530

    Adding latest Tokenizer API

--- src/predict.py
+++ src/predict.py
@@ -12,10 +12,12 @@ data_reader = DataReader(path.train_path, path.test_path, path.med_path)
 no_of_classes = len(data_reader.int_to_pt)
 data_tokenizer = Tokenize(data_reader, max_words=const.MAX_WORDS, max_len=const.MAX_LENGTH, no_of_classes=no_of_classes)
 
+# with open(str(path.TOKENIZER), 'rb') as pkl_file:
+#     data_tokenizer = pickle.load(pkl_file)
+
 lstm_model = LSTMModel(data_tokenizer, model_json_path=path.MODEL_JSON_PATH, 
                         model_weights_path=path.MODEL_BEST_WEIGHTS_PATH, eval=True)
 
-#lstm_model._load_model()
 X = data_tokenizer.prepare_text(["URINARY SYMPTOMS FOLLOWING OFF LABEL USE OF METHOTREXATE FOR MORPHEA PROFUNDA"])
 pt_id, pt_term = lstm_model.predict(X)
 print(pt_id)
